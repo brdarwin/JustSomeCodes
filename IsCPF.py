78ea7b51b671fb4343c1fe3c0570adb1175df1b3
brdarwin <160612209+brdarwin@users.noreply.github.com>
2024-02-28 22:05:28 -0300

    Add files via upload

--- IsCPF.py
+++ IsCPF.py
@@ -1,21 +1,11 @@
-def auxRange (text,n, n1):
-    for i in range (n,n1):
-        if text[i].isdigit() == False:
-            return False
-    return True
+import re
 
-def IsCPF (text):
-    if len(text) != 14:
-        return False
+def hasCPF (text):
+    cpfRegex = re.compile(r'\d{3}.\d{3}.\d{3}-\d{2}')
+    try:
+        CPF =  cpfRegex.search(text)
+        print ("True: " + CPF.group())
+    except AttributeError:
+       return False
 
-    if text[3] != '.' or text[7] != '.' or text[11] != '-':
-        return False
-    test = auxRange (text,0, 3)
-    if test == True:
-        test = auxRange (text,4, 7)
-    if test == True:
-        test = auxRange (text,8, 11)
-    if test == True:
-        test = auxRange (text,12, 14)
-    return test
 
