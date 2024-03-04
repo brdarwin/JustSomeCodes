809d85eb7c4dc1a4a911a773d32275c6d4a92cdf
brdarwin <160612209+brdarwin@users.noreply.github.com>
2024-03-04 13:23:41 -0300

    Add files via upload

--- IsCPF.py
+++ IsCPF.py
@@ -1,11 +1,10 @@
 import re
+import sys
 
 def hasCPF (text):
     cpfRegex = re.compile(r'\d{3}.\d{3}.\d{3}-\d{2}')
     try:
-        CPF =  cpfRegex.search(text)
-        print ("True: " + CPF.group())
+        CPF =  cpfRegex.findall(text)
+        return print ("True: " + str(CPF))
     except AttributeError:
        return False
-
-
